9132d91adacedc8efc7892817d30be41f4097890
Andri Yadi <an.dri@me.com>
2017-10-02 04:09:12 +0700

    Add support for ESP32 boards. Only UART works for now.

--- examples/ATM90E26_UART/ATM90E26_UART.ino
+++ examples/ATM90E26_UART/ATM90E26_UART.ino
@@ -1,5 +1,5 @@
 #include <energyic_UART.h>
-#if !defined(ARDUINO_ARCH_SAMD)
+#if !defined(ARDUINO_ARCH_SAMD) && !defined(ESP32)
 #include <SoftwareSerial.h>
 #else
 
@@ -29,17 +29,31 @@ SoftwareSerial ATMSerial(11, 13); //RX, TX
 Uart ATMSerial(&sercom1, PIN_SerialATM_RX, PIN_SerialATM_TX, PAD_SerialATM_RX, PAD_SerialATM_TX);
 #endif
 
+#if defined(ESP32)
+#define PIN_SerialATM_RX       19   //RX pin, CHANGE IT according to your board
+#define PIN_SerialATM_TX       23   //TX pin, CHANGE IT according to your board
+HardwareSerial ATMSerial(1);        //1 = just hardware serial number. ESP32 supports 3 hardware serials. UART 0 usually for flashing.
+#endif
+
 ATM90E26_UART eic(&ATMSerial);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\nATM90E26 UART Test Started");
+  
   #if defined(ARDUINO_ARCH_SAMD)
   pinPeripheral(PIN_SerialATM_RX, PIO_SERCOM);
   pinPeripheral(PIN_SerialATM_TX, PIO_SERCOM);
   #endif
+
+  #if defined(ESP32)
+  //Must begin ATMSerial before IC init supplying baud rate, serial config, and RX TX pins
+  ATMSerial.begin(9600, SERIAL_8N1, PIN_SerialATM_RX, PIN_SerialATM_TX);
+  #else
   //Must begin ATMSerial before IC init
   ATMSerial.begin(9600);
+  #endif
+  
   eic.InitEnergyIC();
   delay(1000);
 }
@@ -61,10 +75,20 @@ void loop() {
   Serial.println(eic.GetMeterStatus(),HEX);
   delay(10);
   Serial.print("Voltage:");
+  #if defined(ESP32)
+  //ESP32 or maybe another MCU support formatting float/double value
+  Serial.printf("%.4f V\n", eic.GetLineVoltage());
+  #else
   Serial.println(eic.GetLineVoltage());
+  #endif
   delay(10);
   Serial.print("Current:");
+  #if defined(ESP32)
+  //ESP32 or maybe another MCU support formatting float/double value
+  Serial.printf("%.4f A\n", eic.GetLineCurrent());
+  #else
   Serial.println(eic.GetLineCurrent());
+  #endif
   delay(10);
   Serial.print("Active power:");
   Serial.println(eic.GetActivePower());
